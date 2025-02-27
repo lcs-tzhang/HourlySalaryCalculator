c5c48a4c99d788a2ee92e8d53b8ed64986837926
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-27 08:58:11 -0500

    Initial Commit

---
+++ HourlySalaryCalculator/HourlySalaryCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourlySalaryCalculatorApp.swift
+//  HourlySalaryCalculator
+//
+//  Created by xinyu zhang on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct HourlySalaryCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
